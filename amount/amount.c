abeee6de99b0c6e3fdfcf0548e8722cd8f7c4245
Wang <you@example.com>
2016-11-10 09:25:55 -0500

    amount

--- amount/amount.c
+++ amount/amount.c
@@ -4,6 +4,7 @@
 int main()
 {
     /*金额以分为单位*/
+    /*
     long orderamount=1289;
     long Amount;
 
@@ -18,18 +19,21 @@ int main()
     double setamount = (double)orderamount/100 - fee;
     printf("setamount: %f\n", setamount);
 
-    AB_ROUND_01(&setamount, 1);     //小数点后保留几位有效值(不4舍五入)
+    AB_ROUND_01(&setamount, 1);     //小数点后保留几位有效值(不4舍五入), 参数不能为0
     printf("setamount: %f\n", setamount);
 
     //double转long型(如果double型等于 3.9, long型为 3,  所以要加0.5再转)
     Amount = setamount*100 + 0.5;
     printf("Amount: %d\n", Amount);
-    /*
-    long Amount = 99999;
-    double fee = (double)Amount*6/10000;
-    printf("fee : %f\n", fee);
-    AB_ROUND_01(&fee, 2);
-    printf("fee : %f\n", fee);
     */
+    double amount = 59.84;   //小数点后是9， 或4舍五入后是9，  AB_ROUND_01 也会进位， 其他情况舍去
+    AB_ROUND_01(&amount, 0);
+    printf("Amount: %f\n", amount);
+
+
+    //直接全舍去  用double赋值给long就可以了
+    long amount2 = amount;
+    printf("amount2: %d\n", amount2);
+
     return 0;
 }
